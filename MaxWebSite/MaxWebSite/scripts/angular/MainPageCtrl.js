29dbbfdc5953e75efa4d458a41318501a14a263d
--repalce-all <eyad@eyad.com>
2015-06-03 02:18:31 +0300

    Add console.log

--- MaxWebSite/MaxWebSite/scripts/angular/MainPageCtrl.js
+++ MaxWebSite/MaxWebSite/scripts/angular/MainPageCtrl.js
@@ -8,3 +8,63 @@ function ($scope, $http) {
       });
     
 }]);
+
+
+app.controller('eyy', ['$scope', '$http',
+    function ($scope, $http) {
+        $scope.NumOfAdds = 0;
+        $scope.NumOfSide = 0;
+        $scope.NumOfVis = 0;
+        $scope.NumOfAdv = 0;
+        $scope.NewVisters = 0;
+        $scope.NewAdds = 0;
+      
+        $http.get('/GetAllGroups?format=json')
+            .success(function (response) {
+                console.log($scope.groups);
+                $scope.NumOfAdds = response;
+            });
+
+        $http.get('/GetAllGroups?format=json')
+         .success(function (response) {
+             console.log($scope.groups);
+             $scope.NumOfSide = response;
+         });
+
+        $http.get('/GetAllGroups?format=json')
+      .success(function (response) {
+          console.log($scope.groups);
+          $scope.NumOfVis = response;
+      });
+
+        $http.get('/GetAllGroups?format=json')
+      .success(function (response) {
+          console.log($scope.groups);
+          $scope.NumOfAdv = response;
+      });
+
+        $http.get('/GetAllGroups?format=json')
+    .success(function (response) {
+        console.log($scope.groups);
+        $scope.NewVisters0 = response;
+    });
+
+ 
+        $scope.NiVist = ($scope.NewVisters / $scope.NumOfVis) * 100;
+       
+
+
+        $http.get('/GetAllGroups?format=json')
+.success(function (response) {
+    console.log($scope.groups);
+    $scope.NewAdds = response;
+});
+
+
+        $scope.NiAdds = ($scope.NewAdds / $scope.NumOfAdds) * 100;
+
+
+
+
+
+    }]);
